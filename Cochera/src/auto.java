d6b4aca344a8ae3fec87c2a11a456dbd28dd0a0a
Jose Luis <joseluissmedinar@gmail.com>
2016-06-26 23:18:26 -0300

    Cochera

---
+++ Cochera/src/auto.java
@@ -0,0 +1,10 @@
+
+public class auto {
+String patente;
+String marca;
+void mostrar (){
+	System.out.println("La patente es: "+patente);
+	System.out.println("Corresponde al auto marca: "+marca);
+}
+
+}
